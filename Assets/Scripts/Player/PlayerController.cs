a3894f9065f466feeb2b9ad5d7e888ffb83d57e0
HangyeolChoi <choihg14@naver.com>
2024-10-28 10:45:24 +0900

    [feat] Player Input - 이동,점프,카메라 이동 구현
    PlayerController 구현
    점프할 때 한번 더 점프 버튼을 누를 시 키보드 누른 방향으로 대시(앞구르기) 구현

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed = 5f;
+    public float jumpPower = 80f;
+    public float rollSpeed = 8f;
+    private Vector2 curMovementInput;
+    private bool isRolled = false;
+
+    [Header("Look")]
+    public Transform cameraContainer;
+    [SerializeField] private float minXLook;
+    [SerializeField] private float maxXLook;
+    [SerializeField] private float lookSensitivity;
+    private float camCurXRot;
+    private Vector2 mouseDelta;
+    public bool canLook = true;
+
+    [SerializeField] private LayerMask groundLayerMask;
+    private Rigidbody _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (IsGrounded())
+        {
+            Move();
+            isRolled = false;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        Look();
+    }
+
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            curMovementInput = context.ReadValue<Vector2>();
+        }
+        else if (context.phase == InputActionPhase.Canceled)
+        {
+            curMovementInput = Vector2.zero;
+        }
+    }
+
+    private void Move()
+    {
+        Vector3 dir = transform.forward * curMovementInput.y + transform.right * curMovementInput.x;
+        dir *= moveSpeed;
+        dir.y = _rigidbody.velocity.y;
+
+        _rigidbody.velocity = dir;
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        mouseDelta = context.ReadValue<Vector2>();
+    }
+
+    private void Look()
+    {
+        camCurXRot += mouseDelta.y * lookSensitivity;
+        camCurXRot = Mathf.Clamp(camCurXRot, minXLook, maxXLook);
+        cameraContainer.localEulerAngles = new Vector3(-camCurXRot, 0, 0);
+
+        transform.eulerAngles += new Vector3(0, mouseDelta.x * lookSensitivity, 0);
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started && IsGrounded())
+        {
+            _rigidbody.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+        }
+
+        if (context.phase == InputActionPhase.Started && !IsGrounded() && !isRolled)        // 2단 점프시 구르기
+        {
+            Vector3 dir = transform.forward * curMovementInput.y + transform.right * curMovementInput.x;
+            dir *= rollSpeed;
+            dir.y = _rigidbody.velocity.y;
+            _rigidbody.velocity = dir;
+            isRolled = true;
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        Ray[] rays = new Ray[4]
+        {
+            new Ray(transform.position + transform.forward * 0.2f + transform.up * 0.01f, Vector3.down),
+            new Ray(transform.position - transform.forward * 0.2f + transform.up * 0.01f, Vector3.down),
+            new Ray(transform.position + transform.right * 0.2f + transform.up * 0.01f, Vector3.down),
+            new Ray(transform.position - transform.right * 0.2f + transform.up * 0.01f, Vector3.down)
+        };
+
+        for(int i=0; i<rays.Length; i++)
+        {
+            if (Physics.Raycast(rays[i], 0.1f, groundLayerMask))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
